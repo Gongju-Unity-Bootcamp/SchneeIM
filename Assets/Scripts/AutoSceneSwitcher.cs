9f9fb39f16e3a2b050b224e01394f5a22aa3ac57
yangseolhwa <148856593+yangseolhwa@users.noreply.github.com>
2024-02-22 11:35:57 +0900

    작업한 씬 수정
    작업한 씬과 스크립트를 수정했다.

--- Assets/Scripts/AutoSceneSwitcher.cs
+++ Assets/Scripts/AutoSceneSwitcher.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class AutoSceneSwitcher : MonoBehaviour
 {
     public float delayInSeconds = 20f; // 씬 전환까지의 대기 시간 (초)
-
+    public int sceneNum;
     private void Start()
     {
         // delayInSeconds 이후에 다음 씬으로 전환합니다.
@@ -14,6 +14,6 @@ public class AutoSceneSwitcher : MonoBehaviour
     private void LoadNextScene()
     {
         // 다음 씬을 로드합니다.
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(sceneNum);
     }
 }
