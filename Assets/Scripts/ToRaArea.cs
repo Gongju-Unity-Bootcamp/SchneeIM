0f0aa0799153ae7618ecd0264d0a20d914432f5e
Leslie Kim <kimxesxie@gmail.com>
2024-02-27 09:12:23 +0900

    modified subtitles ver.
    Modified subtitle speed(in Pyramid intro)

--- Assets/Scripts/ToRaArea.cs
+++ Assets/Scripts/ToRaArea.cs
@@ -6,7 +6,7 @@ public class ToRaArea : MonoBehaviour
     // 전환할 씬의 이름
     public string nextSceneName = "3_RaArea";
     // 전환까지의 대기 시간 (초)
-    public float delayInSeconds = 40f;
+    public float delayInSeconds = 39f;
 
     // Start 함수에서 지연 후 씬 전환을 호출합니다.
     void Start()
